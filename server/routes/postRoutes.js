e05a7d3d7352e784ebea3a099e3e3b1e378d717b
Saurav Suman <sauravsuman5980@gmail.com>
2023-02-08 01:27:04 +0530

    Added console log to json

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -19,12 +19,10 @@ router.route("/").get(async (req, res) => {
     const posts = await Post.find({});
     res.status(200).json({ success: true, data: posts });
   } catch (err) {
-    res
-      .status(500)
-      .json({
-        success: false,
-        message: "Fetching posts failed, please try again",
-      });
+    res.status(500).json({
+      success: false,
+      message: "Fetching posts failed, please try again",
+    });
   }
 });
 
@@ -39,14 +37,20 @@ router.route("/").post(async (req, res) => {
       photo: photoUrl.url,
     });
 
-    res.status(200).json({ success: true, data: newPost });
-  } catch (err) {
     res
-      .status(500)
+      .status(200)
       .json({
-        success: false,
-        message: "Unable to create a post, please try again",
+        success: true,
+        data: newPost,
+        name: name,
+        prompt: prompt,
+        photo: photo,
       });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Unable to create a post, please try again",
+    });
   }
 });
 
